485cfa0f5f8cbcbbb6f7627fb7535ad6a2faae98
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2025-06-28 01:12:00 +0530

    dsa loops01

---
+++ index.js
@@ -0,0 +1,32 @@
+// for(i=0; i>0; i++){
+//     console.log("hello world " + i);  
+// } 
+// print odd number from arr
+// for loop
+
+/***
+ * let arr = [10,6,2,5,8,3,80]
+let length = arr.length;
+for( let i =0 ; i<length ; i++){
+    let even  = arr[i] % 2 != 0
+    if(even){
+        console.log('  arr[i] ',   arr[i]);
+    }
+}
+ */
+
+// while loop
+
+// let i = 0
+//  while(i<5){
+//      i++;
+//     console.log('HW');
+//  }
+// let arr = [10,6,2,5,8,3,80]
+// while(i<arr.length){
+//     if(arr[i] % 2 == 0){
+//         console.log(arr[i]);
+//     }
+//     i++;
+// }
+
